6213d2f86f95847c5dc92739a9db567cf848dfde
IS Choi <insil321@hotmail.com>
2018-09-26 11:07:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num%3==0 
+  if num/3 == 0 
     puts "Fizz"
-elsif num%5==0 
+elsif num/5==0 
     puts "Buzz"
-elsif num%3==0 && num%5==0 
+elsif num/3==0 && num/5==0 
   puts "Fizzbuzz"
 else
   puts "nil"
